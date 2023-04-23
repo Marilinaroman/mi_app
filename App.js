6afa1f7fce682bdbbd470a2a574eac41f67b069a
Marilina Roman <roman.marilinaf@gmail.com>
2023-04-23 13:40:03 -0300

    inicializo repositorio

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TextInput, View, Button} from 'react-native';
+import {styles} from './style/style'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Ingrese un dato' style={styles.input}/>
+        <Button title='agregar'/>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
